ba5e7f3b68a678790c52e3a729bbbce1485721da
Daichi Hioki <hioki.daichi@gmail.com>
2025-11-24 13:07:36 +0900

    Update

--- init.lua
+++ init.lua
@@ -563,3 +563,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.tabstop = 4
   end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
